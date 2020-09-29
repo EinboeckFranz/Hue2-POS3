531f031f9c03491b03af8e1a01ef752b514adc13
Franz Einböck <Einboeck.Franz@sus.htl-grieskirchen.at>
2020-09-29 11:19:47 +0200

    Added Constructor, also added Methods run and main

--- src/pos3/feinboeck18/HalloJavamitForEach.java
+++ src/pos3/feinboeck18/HalloJavamitForEach.java
@@ -1,5 +1,22 @@
 package pos3.feinboeck18;
 
+import java.util.Arrays;
+import java.util.List;
+
 public class HalloJavamitForEach {
-    
+    private List<String> liste;
+
+    public HalloJavamitForEach(String[] arrayListe) {
+        this.liste = Arrays.asList(arrayListe.clone());
+    }
+
+    public void run() {
+        for (String s : liste)
+            System.out.println(s);
+    }
+
+    public static void main(String[] args) {
+        HalloJavamitForEach main = new HalloJavamitForEach(new String[]{"String1", "String2", "String3", "String4"});
+        main.run();
+    }
 }
