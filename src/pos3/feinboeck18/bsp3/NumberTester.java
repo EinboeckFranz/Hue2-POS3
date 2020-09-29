b7eeb32cc4f9f79018cbadb23ef2d9dedbb5d5b2
Franz Einböck <Einboeck.Franz@sus.htl-grieskirchen.at>
2020-09-29 11:49:28 +0200

    Added a Constructor and Methods (setOddEvenTester, setPrimeTester, setPalindromeTester, testFile), also created a inner class interface called NumberTest

---
+++ src/pos3/feinboeck18/bsp3/NumberTester.java
@@ -0,0 +1,28 @@
+package pos3.feinboeck18.bsp3;
+
+public class NumberTester {
+    //...
+
+    public NumberTester(String path) {
+        //TODO
+    }
+
+    public void setOddEvenTester(NumberTest oddTester) {
+        //TODO
+    }
+
+    public void setPrimeTester(NumberTest primeTester) {
+        //TODO
+    }
+
+    public void setPalindromeTester(NumberTest palindromeTester) {
+        //TODO
+    }
+
+    public void testFile() {
+        //TODO
+    }
+}
+interface NumberTest {
+    boolean testNumber(int number);
+}
