21c76bb22f8ff5e7de77dae857adb0f6d3241b0b
Franz Einböck <Einboeck.Franz@sus.htl-grieskirchen.at>
2020-09-29 12:57:14 +0200

    Added a Constructor and Methods (setOddEvenTester, setPrimeTester, setPalindromeTester, testFile), also created a inner class interface called NumberTest

---
+++ src/pos3/feinboeck18/bsp3/Main.java
@@ -0,0 +1,14 @@
+package pos3.feinboeck18.bsp3;
+
+import java.util.stream.IntStream;
+
+public class Main {
+    public static void main(String[] args) {
+        NumberTester numberTest = new NumberTester("file.txt");
+        numberTest.setOddEvenTester(number -> number % 2 == 0);
+        numberTest.setPrimeTester(number -> number > 1 && IntStream.rangeClosed(2, (int) Math.sqrt(number)).
+                noneMatch(n -> (number % n == 0)));
+        numberTest.setPalindromeTester(number -> number == Integer.reverse(number));
+        numberTest.testFile();
+    }
+}
