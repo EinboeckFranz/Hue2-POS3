a5127d5289ef33a2142d71527d367a48e2cc0656
Franz Einböck <Einboeck.Franz@sus.htl-grieskirchen.at>
2020-10-02 13:56:26 +0200

    Finished my actions in 'bsp3' package, now editing Bsp 4

--- src/pos3/feinboeck18/bsp3/Main.java
+++ src/pos3/feinboeck18/bsp3/Main.java
@@ -1,15 +1,22 @@
 package pos3.feinboeck18.bsp3;
 
+import java.util.Scanner;
 import java.util.stream.IntStream;
 
 public class Main {
     public static void main(String[] args) {
+        run();
+    }
+
+    public static void run() {
         NumberTester numberTest = new NumberTester("file.txt");
         numberTest.setOddEvenTester(number -> number % 2 == 0);
         numberTest.setPrimeTester(number -> number > 1 && IntStream.rangeClosed(2, (int) Math.sqrt(number)).
                 noneMatch(n -> (number % n == 0)));
-        numberTest.setPalindromeTester(number -> number == Integer.reverse(number));
+        numberTest.setPalindromeTester(number -> {
+            String stringValue = String.valueOf(number);
+            return stringValue.equals(new StringBuilder(stringValue).reverse().toString());
+        });
         numberTest.testFile();
-
     }
 }
--- src/pos3/feinboeck18/bsp3/NumberTester.java
+++ src/pos3/feinboeck18/bsp3/NumberTester.java
@@ -1,28 +1,73 @@
 package pos3.feinboeck18.bsp3;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
 public class NumberTester {
-    //...
+    private final File file;
+    private NumberTest oddTester;
+    private NumberTest primeTester;
+    private NumberTest palindromeTester;
 
     public NumberTester(String path) {
-        //TODO
+        this.file = new File(path);
     }
 
     public void setOddEvenTester(NumberTest oddTester) {
-        //TODO
+        this.oddTester = oddTester;
     }
 
     public void setPrimeTester(NumberTest primeTester) {
-        //TODO
+        this.primeTester = primeTester;
     }
 
     public void setPalindromeTester(NumberTest palindromeTester) {
-        //TODO
+        this.palindromeTester = palindromeTester;
     }
 
     public void testFile() {
-        //TODO
+        try {
+            Scanner sc = new Scanner(file);
+            int howMuchOperations = Integer.parseInt(sc.nextLine().trim());
+
+            if(howMuchOperations < 1)
+                return;
+
+            for (int i = 1; i <= howMuchOperations; i++) {
+                if(sc.hasNextLine()) {
+                    String[] arr = sc.nextLine().split(" ");
+                    if(arr.length > 2) {
+                        for (int j = 1; j < arr.length; j++)
+                            operationalSwitch(Integer.parseInt(arr[0].trim()), Integer.parseInt(arr[j].trim()));
+                    } else if(arr.length == 2)
+                        operationalSwitch(Integer.parseInt(arr[0].trim()), Integer.parseInt(arr[1].trim()));
+                } else
+                    break;
+            }
+        } catch (FileNotFoundException e) {
+            System.out.println("This path could not be found");
+        }
+    }
+
+    private void operationalSwitch(int operationNumber, int numberToTest) {
+        switch (operationNumber) {
+            case 1:
+                System.out.println(oddTester.testNumber(numberToTest) ? "EVEN" : "ODD");
+            break;
+            case 2:
+                System.out.println(primeTester.testNumber(numberToTest) ? "PRIME" : "NO PRIME");
+            break;
+            case 3:
+                System.out.println(palindromeTester.testNumber(numberToTest) ? "PALINDROME" : "NO PALINDROME");
+            break;
+            default:
+                System.out.println("*** Unknown Operation! ***");
+            break;
+        }
     }
 }
+
 interface NumberTest {
     boolean testNumber(int number);
 }
