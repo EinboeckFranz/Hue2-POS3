7bc71ca5966ac43b9d6b7fc1083c1b57af5430af
Franz Einböck <Einboeck.Franz@sus.htl-grieskirchen.at>
2020-10-04 01:35:35 +0200

    Added class logic to Calculator-Classes

--- src/pos3/feinboeck18/bsp4/ComplexCalculator.java
+++ src/pos3/feinboeck18/bsp4/ComplexCalculator.java
@@ -1,5 +1,27 @@
 package pos3.feinboeck18.bsp4;
 
 public class ComplexCalculator extends AbstractCalculator {
-    //TODO
+    public ComplexCalculator(CalculationOperation add, CalculationOperation subtract, CalculationOperation multiply, CalculationOperation divide) {
+        super(add, subtract, multiply, divide);
+    }
+
+    @Override
+    public Number add(Number a, Number b) {
+        return add.calc(a, b);
+    }
+
+    @Override
+    public Number subtract(Number a, Number b) {
+        return subtract.calc(a, b);
+    }
+
+    @Override
+    public Number multiply(Number a, Number b) {
+        return multiply.calc(a, b);
+    }
+
+    @Override
+    public Number divide(Number a, Number b) {
+        return divide.calc(a, b);
+    }
 }
